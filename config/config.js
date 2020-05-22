ecbb8835c0c9527cab93fa0dbfd8853ee48375bc
Miguel <mespinosa@gnobit.com>
2020-05-21 22:55:42 -0500

    Fin de la seccion 12

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.CLIENT_ID = '20690273984-1us2v3q07vblfbn9dmtu7o8iqg5h7v72.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'YjBdpQ1GCpbReP3lFcbH5iYM';
